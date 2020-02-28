8e6da66699ea2b23b72088fe09612f91cd532bbe
Garry1980 <ludingv@yandex.ru>
2020-02-28 21:27:51 +0500

    First step

---
+++ mern/client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { useRoutes } from './routes';
+import { useAuth } from './hooks/auth.hook';
+import { AuthContext } from './context/AuthContext';
+
+import 'materialize-css';
+
+function App() {
+
+  const { login, logout, token, userId } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
+
+  return (
+    <AuthContext.Provider value={{
+      token, userId, login, logout, isAuthenticated
+    }}>
+      <BrowserRouter>
+        <div className="container">
+          {routes}
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
