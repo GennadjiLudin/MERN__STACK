512eaf65809619d1a513008a94803e24a1d42bd3
Garry1980 <ludingv@yandex.ru>
2020-02-29 19:06:41 +0500

    Test Task

--- mern/client/src/pages/CreatePage.js
+++ mern/client/src/pages/CreatePage.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 const CreatePage = () => {
+    const [link, setLink] = useState('');
+    const {request} = useHttp();
+
+    const history = useHistory();
+    const auth = useContext(AuthContext);
+    
+    useEffect(() => {
+        window.M.updateTextFields();
+    }, [])
+
+    const pressHandler = async event => {
+        if (event.key === 'Enter') {
+            try {
+                const data = await request('/api/link/generate', 'POST', { from: link }, {
+                    Authorization: `Bearer ${auth.token}`,
+                });
+                history.push(`/detail/${data.link._id}`);
+                
+            } catch (error) {}
+        }
+    }
+
     return (
-        <div>
-            <h1>Create Page</h1>
+        <div className="row">
+            <div className="col s8 offset-s2" style={{ paddingTop: '2rem' }}>
+                <div className="input-field">
+                    <input 
+                        placeholder="Link"
+                        id="link" 
+                        type="text"
+                        name="link"
+                        value={link}
+                        onChange={e => setLink(e.target.value)}
+                        onKeyPress={pressHandler}
+                    />
+                    <label htmlFor="link">Paste link</label>
+                </div>
+            </div>
         </div>
     );
 }
--- mern/routes/auth.routes.js
+++ mern/routes/auth.routes.js
@@ -80,7 +80,7 @@ router.post(
         const token = jwt.sign(
             { userId: user.id },
             config.get('jwtSecret'),
-            { expiresIn: '1h' }
+            { expiresIn: '8h' }
         );
 
         res.json({ token, userId: user.id });
